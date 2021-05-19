dcfde45d6383a7ecba0bfd087591670d53ca9930
Anchobies <anthonychung97@gmail.com>
2021-05-18 21:58:26 -0400

    commit tree 214b49df375ebd81729e638f5ce7540534e685dd

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+var superbowlWin = record => {
+    const game = record.find(function(obj) {
+        return obj.result === "W";
+    });
 
+    if (game != undefined) {
+        return game.year;
+    }
+
+    return game;
+}
